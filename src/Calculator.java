d44dd334544a6a0bcd3816b854b7cef8cf1f661f
M Raihan Aryadevin <93025188+MRaihan21@users.noreply.github.com>
2023-02-03 15:28:13 +0700

    merevisi ulang kode programnya
    mengecilkan ukuran jframe, menganti warna background, menambahkan jtextfield baru di atas, membuat tombol baru modulus dll

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,54 +8,68 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        new kalkulator();        
+        new kalkulator();
 
     }
-
 }
 
 class kalkulator implements ActionListener {
 
     JFrame frame;
-    JTextField textField;
+    JTextField textField, text;
     JButton[] numberButtons = new JButton[10];
-    JButton[] functionButtons = new JButton[9];
+    JButton[] functionButtons = new JButton[10];
     JButton addButton, subButton, mulButton, divButton;
-    JButton decButton, equButton, delButton, clrButton, negButton;
+    JButton decButton, equButton, delButton, clrButton, negButton, persenButton;
     JPanel panel;
-    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+    Font myFont = new Font("Inter", Font.BOLD, 22);
+    Font myFont2 = new Font("Inter", Font.BOLD, 20);
+    //new Font("Ink Free", Font.BOLD, 30);
     double num1 = 0, num2 = 0, result = 0;
     char operator;
+    int i;
 
     public kalkulator() {
 
         frame = new JFrame("Calculator");
-        frame.setSize(420, 600);
+        frame.setSize(335, 525);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setBackground(Color.black);
+        frame.getContentPane().setBackground(Color.BLACK);
         frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+
         frame.setLayout(null);
-        
+
 //        ImageIcon icon = new ImageIcon("C:\\Users\\A C E R\\Downloads\\JavaPicture\\calculator.png");
 //        frame.setIconImage(icon.getImage());
         frame.setIconImage(new ImageIcon("C:\\\\Users\\\\A C E R\\\\Downloads\\\\JavaPicture\\\\calculator.png").getImage());
-                
-        textField = new JTextField();
-        textField.setBounds(50, 75, 300, 50);
+
+        textField = new JTextField("0");
+        textField.setBounds(10, 50, 300, 50);
         textField.setFont(myFont);
         textField.setForeground(Color.black);
-        textField.setBackground(new Color(10,255,25));
-        textField.setEditable(true);
+        textField.setBackground(Color.green);
+        textField.setEditable(false);
+
+        text = new JTextField();
+        text.setBounds(230, 20, 80, 25);
+        text.setBackground(Color.black);
+        text.setForeground(Color.white);
+        text.setBorder(null);
+        text.addActionListener(this);
+        text.setEditable(false);
+        frame.add(text);
 
         addButton = new JButton("+");
         subButton = new JButton("-");
-        mulButton = new JButton("*");
+        mulButton = new JButton("x");
         divButton = new JButton("/");
-        decButton = new JButton(".");
+        decButton = new JButton(",");
         equButton = new JButton("=");
-        delButton = new JButton("D");
-        clrButton = new JButton("C");
-        negButton = new JButton("(-)");
+        delButton = new JButton("DEL");
+        clrButton = new JButton("CA");
+        negButton = new JButton("(+/-)");
+        persenButton = new JButton("%");
 
         functionButtons[0] = addButton;
         functionButtons[1] = subButton;
@@ -66,12 +80,14 @@ class kalkulator implements ActionListener {
         functionButtons[6] = delButton;
         functionButtons[7] = clrButton;
         functionButtons[8] = negButton;
+        functionButtons[9] = persenButton;
 
-        for (int i = 0; i < 9; i++) {
+        for (i = 0; i < 10; i++) {
             functionButtons[i].addActionListener(this);
-            functionButtons[i].setFont(myFont);
-            functionButtons[i].setForeground(new Color(10,255,25));
-            functionButtons[i].setBackground(Color.gray);
+            functionButtons[i].setFont(myFont2);
+            functionButtons[i].setForeground(Color.white);
+            functionButtons[i].setBackground(new Color(44, 44, 44));
+            functionButtons[i].setBorder(BorderFactory.createLineBorder(Color.black));
             functionButtons[i].setFocusable(false);
         }
 
@@ -79,42 +95,45 @@ class kalkulator implements ActionListener {
             numberButtons[i] = new JButton(String.valueOf(i));
             numberButtons[i].addActionListener(this);
             numberButtons[i].setFont(myFont);
-            numberButtons[i].setForeground(Color.white);
-            numberButtons[i].setBackground(Color.gray);
+            numberButtons[i].setForeground(new Color(146, 146, 146)); 
+            numberButtons[i].setBackground(new Color(44, 44, 44));
+            numberButtons[i].setBorder(BorderFactory.createLineBorder(Color.black));
             numberButtons[i].setFocusable(false);
 
         }
 
-        negButton.setBounds(50, 460, 100, 50);
-        delButton.setBounds(150, 460, 100, 50);
-        clrButton.setBounds(250, 460, 100, 50);
-
         panel = new JPanel();
-        panel.setBounds(50, 150, 300, 300);
+        panel.setBounds(12, 115, 300, 370);
         panel.setBackground(Color.black);
-        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        panel.setLayout(new GridLayout(5, 4, 12, 12));
+
+        panel.add(clrButton);
+        panel.add(delButton);
+        panel.add(persenButton);
+        panel.add(divButton);
+
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
 
         panel.add(numberButtons[1]);
         panel.add(numberButtons[2]);
         panel.add(numberButtons[3]);
         panel.add(addButton);
-        panel.add(numberButtons[4]);
-        panel.add(numberButtons[5]);
-        panel.add(numberButtons[6]);
-        panel.add(subButton);
-        panel.add(numberButtons[7]);
-        panel.add(numberButtons[8]);
-        panel.add(numberButtons[9]);
-        panel.add(mulButton);
-        panel.add(decButton);
+
+        panel.add(negButton);
         panel.add(numberButtons[0]);
+        panel.add(decButton);
+        equButton.setBackground(Color.green);
         panel.add(equButton);
-        panel.add(divButton);
 
         frame.add(panel);
-        frame.add(negButton);
-        frame.add(delButton);
-        frame.add(clrButton);                
         frame.add(textField);
 
         frame.setVisible(true);
@@ -124,37 +143,49 @@ class kalkulator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        for (int i = 0; i < 10; i++) {
+        for (i = 0; i < 10; i++) {
             if (e.getSource() == numberButtons[i]) {
-                textField.setText(textField.getText().concat(String.valueOf(i)));                
-            }
+                if (textField.getText().equals("0")) {
+                    textField.setText("");
+                }
+                  textField.setText(textField.getText().concat(String.valueOf(i)));
+            }         
         }
+
         if (e.getSource() == decButton) {
-            textField.setText(textField.getText().concat("."));            
+            textField.setText(textField.getText().concat("."));
         }
         if (e.getSource() == addButton) {
-            num1 = Double.parseDouble(textField.getText());                        
+            num1 = Double.parseDouble(textField.getText());
             operator = '+';
             textField.setText("");
-            
+
+            text.setText(text.getText().concat(String.valueOf(num1)).concat(" + "));
         }
+
         if (e.getSource() == subButton) {
-            num1 = Double.parseDouble(textField.getText());            
+            num1 = Double.parseDouble(textField.getText());
             operator = '-';
-            textField.setText("");            
+            textField.setText("");
+
+            text.setText(text.getText().concat(String.valueOf(num1)).concat(" - "));
         }
         if (e.getSource() == divButton) {
-            num1 = Double.parseDouble(textField.getText());            
+            num1 = Double.parseDouble(textField.getText());
             operator = '/';
-            textField.setText("");                        
+            textField.setText("");
+
+            text.setText(text.getText().concat(String.valueOf(num1)).concat(" / "));
         }
         if (e.getSource() == mulButton) {
-            num1 = Double.parseDouble(textField.getText());            
+            num1 = Double.parseDouble(textField.getText());
             operator = '*';
-            textField.setText("");            
+            textField.setText("");
+
+            text.setText(text.getText().concat(String.valueOf(num1)).concat(" x "));
         }
         if (e.getSource() == equButton) {
-            num2 = Double.parseDouble(textField.getText());            
+            num2 = Double.parseDouble(textField.getText());
             switch (operator) {
                 case '+' ->
                     result = num1 + num2;
@@ -165,24 +196,36 @@ class kalkulator implements ActionListener {
                 case '/' ->
                     result = num1 / num2;
             }
-            textField.setText(String.valueOf(result));            
+            textField.setText(String.valueOf(result));
+            text.setText(text.getText().concat(String.valueOf(num2)).concat(" = "));
+            num1 = result;
         }
 
         if (e.getSource() == clrButton) {
-            textField.setText("");
+            textField.setText("0");
+            text.setText("");
         }
         if (e.getSource() == delButton) {
+            text.setText("");
             String string = textField.getText();
-            textField.setText("");
-            for (int i = 0; i < string.length() - 1; i++) {
-                textField.setText(textField.getText() + string.charAt(i));
+            if (string.length() > 1) {
+                string = string.substring(0, string.length() - 1);
+                textField.setText(string);
+            } else if (string.length() == 1) {
+                textField.setText("0");
             }
         }
+
         if (e.getSource() == negButton) {
             double temp = Double.parseDouble(textField.getText());
             temp *= -1;
             textField.setText(String.valueOf(temp));
         }
+
+        if (e.getSource() == persenButton) {
+
+        }
+
     }
 
 }
